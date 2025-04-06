6718f18daeb93f521c50924056a0d0d2f50f5d77
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-04-05 21:43:58 -0400

    main

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ console.log({articleCardContainer});
 const articleCards = [];
 
 // fill up article container
-for (let i=0; i<9; i++) {
+for (let i=1; i<9; i++) {
 
     articleCards.push(articleCard)
 
