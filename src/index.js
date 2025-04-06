16b3fdc4b34e6f299aad8a6c7a9232d841bd6783
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-04-05 22:50:33 -0400

    main

--- src/index.js
+++ src/index.js
@@ -25,3 +25,35 @@ articleCards.map(article => {
 })
 
 // work on styling articles so that they display correctly. then we can work on pulling data for each article
+
+// https://inshorts.deta.dev/news?category={category_name}
+
+// https://inshorts.deta.dev/news?category=science
+
+const apiUrlScience = 'https://corsproxy.io/?url=https://inshorts.deta.dev/news?category=science';
+
+// const apiMediaStackScienceUrl = 'http://api.mediastack.com/v1/news?access_key=c9b1d6034c94d5271701ad8923a8f95b&categories=science';
+
+// fetch(apiMediaStackScienceUrl)
+//     .then(res => {
+//         if (!res.ok) {
+//             throw new Error('Network response not ok');
+//         }
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error('Error', error)
+//     });
+
+// https://corsproxy.io/?url=https://example.com
+
+// http://api.mediastack.com/v1/news
+//     ? access_key = c9b1d6034c94d5271701ad8923a8f95b
+//     & categories = science
+
+// http://api.mediastack.com/v1/news?access_key=c9b1d6034c94d5271701ad8923a8f95b&categories=science
+
+
